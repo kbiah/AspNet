dccfae269100c96a2409a978f50ec23b4fa01aaa
Bianca <bianca.kerkhoff@digitaldesk.com.br>
2023-08-23 18:29:20 -0300

    Correção idioma

--- lagrimas/Models/Infra/RegistrarNovoUsuarioViewModel.cs
+++ lagrimas/Models/Infra/RegistrarNovoUsuarioViewModel.cs
@@ -4,21 +4,21 @@ namespace lagrimas.Models.Infra
 {
     public class RegistrarNovoUsuarioViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Preencha esse campo!")]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Preencha esse campo!")]
         [StringLength(100, ErrorMessage = "A {0} precisa ter ao menos {2} e no máximo {1} caracteres de cumprimento.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Senha")]
-
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Preencha esse campo!")]
         [DataType(DataType.Password)]
         [Display(Name = "Confirmar senha")]
         [Compare("Password", ErrorMessage = "Os valores informados para SENHA e CONFIRMAÇÃO não são iguais.")]
-
         public string ConfirmPassword { get; set; }
     }
 }
--- lagrimas/wwwroot/lib/jquery-validate/jquery.validate.js
+++ lagrimas/wwwroot/lib/jquery-validate/jquery.validate.js
@@ -369,9 +369,9 @@ $.extend( $.validator, {
 	},
 
 	messages: {
-		required: "This field is required.",
+		required: "Esse campo precisa ser preenchido.",
 		remote: "Please fix this field.",
-		email: "Please enter a valid email address.",
+		email: "Envie um email valido",
 		url: "Please enter a valid URL.",
 		date: "Please enter a valid date.",
 		dateISO: "Please enter a valid date (ISO).",
