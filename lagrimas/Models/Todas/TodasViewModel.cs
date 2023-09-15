ad14396c620af35036554d395a3f428bf5caeac6
Bianca <bianca.kerkhoff@digitaldesk.com.br>
2023-09-15 16:15:55 -0300

    dashboard

---
+++ lagrimas/Models/Todas/TodasViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace lagrimas.Models.Todas
+{
+    public class TodasViewModel
+    {
+        public lagrimas.Models.Dashboard.DashboardModel DashboardModel { get; set; }
+
+        public IEnumerable<Modelo.Cadastros.Departamento> DepartamentoIndex { get; set; }
+        public Modelo.Cadastros.Departamento DepartamentoModel { get; set; }
+
+        public Modelo.Cadastros.Instituicao InstituicaoModel { get; set; }
+        public IEnumerable<Modelo.Cadastros.Instituicao> InstituicaoIndex { get; set; }
+
+    }
+}
